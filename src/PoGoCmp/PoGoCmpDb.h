7e9bb730e3501eb8651333516dcff8b9e7eb7e40
Stinkfist0 <stinkfist0@gmail.com>
2018-03-21 00:03:57 +0200

    Fix wrongly copy-pasted string literal + NameTest.

--- src/PoGoCmp/PoGoCmpDb.h
+++ src/PoGoCmp/PoGoCmpDb.h
@@ -934,7 +934,7 @@ static const std::string FarfetchdName{ "Farfetch'd" };
 static const std::string HoOhName{ "Ho-Oh" };
 // Cannot use the actual ♀ & ♂ symbols in the literals as those would get corrupted
 // on MSVC unless the file would be saved as UTF-16 LE BOM which I don't want to do.
-static const Utf8::String NidoranFemaleName{ u8"Nidoran\u2642" };
+static const Utf8::String NidoranFemaleName{ u8"Nidoran\u2640" };
 static const Utf8::String NidoranMaleName{ u8"Nidoran\u2642" };
 // - Mime Jr. -> Unknown at the moment, probably MIME_JR
 // - Flabébé -> Unknown at the moment, probably FLABEBE
--- tests/NameTest.cpp
+++ tests/NameTest.cpp
@@ -3,18 +3,24 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <locale>
 
 /// @todo Unicode output
 #define Assert(a, b) \
 if ((a) != (b)) \
 { std::cerr << "'" << a << "' did not map into '" << b << "'\n"; return EXIT_FAILURE; }
+    #include <codecvt>
 
 int main(/*int argc, char **argv*/)
 {
     using namespace PoGoCmp;
-    Assert(PokemonNameToId(u8"nidoran♀"), "NIDORAN_FEMALE");
+    // Would be nice to use e.g. u8"Nidoran♀" here but unfortunately not possible currently
+    // on MSVC without changing to source file encoding to UTF-16 LE BOM.
+    auto nidoranFemale = PoGoCmp::NidoranFemaleName;
+    nidoranFemale[0] = std::tolower(nidoranFemale[0], {});
+    Assert(PokemonNameToId(nidoranFemale), "NIDORAN_FEMALE");
     Assert(PokemonNameToId("Nidoran Female"), "NIDORAN_FEMALE");
-    Assert(PokemonNameToId(u8"nidoran♂"), "NIDORAN_MALE");
+    Assert(PokemonNameToId(PoGoCmp::NidoranMaleName), "NIDORAN_MALE");
     Assert(PokemonNameToId("Nidoran Male"), "NIDORAN_MALE");
     Assert(PokemonNameToId("Farfetch'd"), "FARFETCHD");
     Assert(PokemonNameToId("Mr. Mime"), "MR_MIME");
@@ -22,8 +28,8 @@ int main(/*int argc, char **argv*/)
     Assert(PokemonNameToId("Ho-Oh"), "HO_OH");
     Assert(PokemonNameToId("ho oh"), "HO_OH");
 
-    Assert(PokemonIdToName("nidoran_female"), u8"Nidoran♀");
-    Assert(PokemonIdToName("NIDORAN_MALE"), u8"Nidoran♂");
+    Assert(PokemonIdToName("nidoran_female"), PoGoCmp::NidoranFemaleName);
+    Assert(PokemonIdToName("NIDORAN_MALE"), PoGoCmp::NidoranMaleName);
     Assert(PokemonIdToName("FARFETCHD"), "Farfetch'd");
     Assert(PokemonIdToName("MR_MIME"), "Mr. Mime");
     Assert(PokemonIdToName("HO_OH"), "Ho-Oh");
