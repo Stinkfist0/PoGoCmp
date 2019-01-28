213f71c12f46ef02887080092119e5299a4bbbf0
Stinkfist0 <stinkfist0@gmail.com>
2019-01-28 13:08:28 +0200

    Tweak NameTest.

--- tests/NameTest.cpp
+++ tests/NameTest.cpp
@@ -5,10 +5,14 @@
 #include <iostream>
 #include <locale>
 
-//! @todo Unicode output, AssertNameToId(name, id), AssertIdToName(id, name)
-#define Assert(a, b) \
-if ((a) != (b)) \
-{ std::cerr << "'" << a << "' did not map into '" << b << "'\n"; return EXIT_FAILURE; }
+//! @todo Unicode output
+#define AssertNameToId(a, b) \
+if (PokemonNameToId((a)) != (b)) \
+{ std::cerr << "Name '" << a << "' did not map into ID '" << b << "'\n"; return EXIT_FAILURE; }
+
+#define AssertIdToName(a, b) \
+if (PokemonIdToName((a)) != (b)) \
+{ std::cerr << "ID '" << a << "' did not map into name '" << b << "'\n"; return EXIT_FAILURE; }
 
 int main(/*int argc, char **argv*/)
 {
@@ -17,47 +21,47 @@ int main(/*int argc, char **argv*/)
     // on MSVC without changing to source file encoding to UTF-16 LE BOM.
     auto nidoranFemale = PoGoCmp::NidoranFemaleName;
     nidoranFemale[0] = std::tolower(nidoranFemale[0], {});
-    Assert(PokemonNameToId(nidoranFemale), "NIDORAN_FEMALE");
-    Assert(PokemonNameToId("Nidoran Female"), "NIDORAN_FEMALE");
-    Assert(PokemonNameToId(PoGoCmp::NidoranMaleName), "NIDORAN_MALE");
-    Assert(PokemonNameToId("Nidoran Male"), "NIDORAN_MALE");
-    Assert(PokemonNameToId("Farfetch'd"), "FARFETCHD");
-    Assert(PokemonNameToId("Mr. Mime"), "MR_MIME");
-    Assert(PokemonNameToId("mr mime"), "MR_MIME");
-    Assert(PokemonNameToId("Ho-Oh"), "HO_OH");
-    Assert(PokemonNameToId("ho oh"), "HO_OH");
-    Assert(PokemonNameToId("Unown !"), "UNOWN_EXCLAMATION_POINT");
-    Assert(PokemonNameToId("Unown ?"), "UNOWN_QUESTION_MARK");
-    Assert(PokemonNameToId("spinda 1"), "SPINDA_00");
-    Assert(PokemonNameToId("SPINDA 2"), "SPINDA_01");
-    Assert(PokemonNameToId("Spinda 3"), "SPINDA_02");
-    Assert(PokemonNameToId("Spinda 4"), "SPINDA_03");
-    Assert(PokemonNameToId("Spinda 5"), "SPINDA_04");
-    Assert(PokemonNameToId("Spinda 6"), "SPINDA_05");
-    Assert(PokemonNameToId("Spinda 7"), "SPINDA_06");
-    Assert(PokemonNameToId("Spinda 8"), "SPINDA_07");
-    Assert(PokemonNameToId("porygonz"), "PORYGON_Z");
-    Assert(PokemonNameToId("porygon z"), "PORYGON_Z");
-    Assert(PokemonNameToId("mime jr"), "MIME_JR");
-    Assert(PokemonNameToId("mime jr."), "MIME_JR");
+    AssertNameToId(nidoranFemale, "NIDORAN_FEMALE");
+    AssertNameToId("Nidoran Female", "NIDORAN_FEMALE");
+    AssertNameToId(PoGoCmp::NidoranMaleName, "NIDORAN_MALE");
+    AssertNameToId("Nidoran Male", "NIDORAN_MALE");
+    AssertNameToId("Farfetch'd", "FARFETCHD");
+    AssertNameToId("Mr. Mime", "MR_MIME");
+    AssertNameToId("mr mime", "MR_MIME");
+    AssertNameToId("Ho-Oh", "HO_OH");
+    AssertNameToId("ho oh", "HO_OH");
+    AssertNameToId("Unown !", "UNOWN_EXCLAMATION_POINT");
+    AssertNameToId("Unown ?", "UNOWN_QUESTION_MARK");
+    AssertNameToId("spinda 1", "SPINDA_00");
+    AssertNameToId("SPINDA 2", "SPINDA_01");
+    AssertNameToId("Spinda 3", "SPINDA_02");
+    AssertNameToId("Spinda 4", "SPINDA_03");
+    AssertNameToId("Spinda 5", "SPINDA_04");
+    AssertNameToId("Spinda 6", "SPINDA_05");
+    AssertNameToId("Spinda 7", "SPINDA_06");
+    AssertNameToId("Spinda 8", "SPINDA_07");
+    AssertNameToId("porygonz", "PORYGON_Z");
+    AssertNameToId("porygon z", "PORYGON_Z");
+    AssertNameToId("mime jr", "MIME_JR");
+    AssertNameToId("mime jr.", "MIME_JR");
 
-    Assert(PokemonIdToName("nidoran_female"), PoGoCmp::NidoranFemaleName);
-    Assert(PokemonIdToName("NIDORAN_MALE"), PoGoCmp::NidoranMaleName);
-    Assert(PokemonIdToName("FARFETCHD"), "Farfetch'd");
-    Assert(PokemonIdToName("MR_MIME"), "Mr. Mime");
-    Assert(PokemonIdToName("HO_OH"), "Ho-Oh");
-    Assert(PokemonIdToName("UNOWN_EXCLAMATION_POINT"), "Unown !");
-    Assert(PokemonIdToName("UNOWN_QUESTION_MARK"), "Unown ?");
-    Assert(PokemonIdToName("SPINDA_00"), "Spinda 1");
-    Assert(PokemonIdToName("SPINDA_01"), "Spinda 2");
-    Assert(PokemonIdToName("SPINDA_02"), "Spinda 3");
-    Assert(PokemonIdToName("SPINDA_03"), "Spinda 4");
-    Assert(PokemonIdToName("SPINDA_04"), "Spinda 5");
-    Assert(PokemonIdToName("SPINDA_05"), "Spinda 6");
-    Assert(PokemonIdToName("SPINDA_06"), "Spinda 7");
-    Assert(PokemonIdToName("SPINDA_07"), "Spinda 8");
-    Assert(PokemonIdToName("MIME_JR"), "Mime Jr.");
-    Assert(PokemonIdToName("PORYGON_Z"), "Porygon Z");
+    AssertIdToName("nidoran_female", PoGoCmp::NidoranFemaleName);
+    AssertIdToName("NIDORAN_MALE", PoGoCmp::NidoranMaleName);
+    AssertIdToName("FARFETCHD", "Farfetch'd");
+    AssertIdToName("MR_MIME", "Mr. Mime");
+    AssertIdToName("HO_OH", "Ho-Oh");
+    AssertIdToName("UNOWN_EXCLAMATION_POINT", "Unown !");
+    AssertIdToName("UNOWN_QUESTION_MARK", "Unown ?");
+    AssertIdToName("SPINDA_00", "Spinda 1");
+    AssertIdToName("SPINDA_01", "Spinda 2");
+    AssertIdToName("SPINDA_02", "Spinda 3");
+    AssertIdToName("SPINDA_03", "Spinda 4");
+    AssertIdToName("SPINDA_04", "Spinda 5");
+    AssertIdToName("SPINDA_05", "Spinda 6");
+    AssertIdToName("SPINDA_06", "Spinda 7");
+    AssertIdToName("SPINDA_07", "Spinda 8");
+    AssertIdToName("MIME_JR", "Mime Jr.");
+    AssertIdToName("PORYGON_Z", "Porygon Z");
 
     if (PoGoCmp::PokemonByIdName("SPINDA_08").number != 0)
     {
